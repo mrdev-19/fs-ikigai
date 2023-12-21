c48d62b220442426bb2211bca4a2d1e41b8cb043
Chanakya Devendra <101084514+mrdev-19@users.noreply.github.com>
2023-12-21 14:29:01 +0530

    Update ikigai_pillow.py

--- ikigai_pillow.py
+++ ikigai_pillow.py
@@ -8,27 +8,29 @@ def add_text_to_image(input_image_path, output_image_path, texts, positions, fon
         if font_path:
             font = ImageFont.truetype(font_path, font_size)
         else:
-            font = ImageFont.truetype("arial.ttf", font_size)
+            font = ImageFont.truetype("Gaegu.ttf", font_size)
     except IOError:
-        font = ImageFont.truetype("arial.ttf", font_size)
+        font = ImageFont.truetype("Gaegu.ttf", font_size)
     
     for text, position in zip(texts, positions):
-        draw.text(position, text, font=font, fill=font_color)
+        draw.text(position, text, font=font, fill="#4B4649")
     image.save(output_image_path)
-
-def chatgpt_model(responses):
-    #we use the responses to genrate the 4 values for putting in our pic
-    return responses
 
 def pic_write(responses_from_user):
     input_image_path = "ikigai.jpeg"
     output_image_path = "ikigai_with_text.jpeg"
     #we can add the text we want to add here
-    texts_to_add=chatgpt_model(responses_from_user)
-    #positions fo the texts def image size is 640 x 640
-    positions = [(190, 210), (400, 210), (190, 410), (400, 410)]
-    font_size = 20
+    texts_to_add=responses_from_user
+    #positions fo the texts def image size is 1080 x 1080
+    custom_font_path = "Gaegu-bold.ttf"
+    texts_to_add[0]=texts_to_add[0]+"'s"
+    W,H=1080,1080
+    msg=texts_to_add[0]
+    im = Image.new("RGBA",(W,H),"yellow")
+    draw = ImageDraw.Draw(im)
+    w, h = draw.textsize(msg)
+    positions = [(((W-w)/2)-20,505),(480,180),(170,500),(480,800),(750,500),(350,430),(610,430),(350,630),(610,630)]
+    font_size = 30
     #if we have another font we can use this
-    custom_font_path = "Arial.ttf"
     add_text_to_image(input_image_path, output_image_path, texts_to_add, positions, font_size, font_path=custom_font_path)
     #we can change the text size , font , and positions as needed 
