781c68be941a35fc016923648fb34a1b78fd6345
Chanakya Devendra <101084514+mrdev-19@users.noreply.github.com>
2023-12-21 14:44:14 +0530

    Update ikigai_pillow.py

--- ikigai_pillow.py
+++ ikigai_pillow.py
@@ -24,13 +24,16 @@ def pic_write(responses_from_user):
     #positions fo the texts def image size is 1080 x 1080
     custom_font_path = "Gaegu-bold.ttf"
     texts_to_add[0]=texts_to_add[0]+"'s"
-    W,H=1080,1080
-    msg=texts_to_add[0]
-    im = Image.new("RGBA",(W,H),"yellow")
+    W, H = 1080, 1080
+    msg = texts_to_add[0]
+    im = Image.new("RGBA", (W, H), "yellow")
     draw = ImageDraw.Draw(im)
-    w, h = draw.textsize(msg)
+    font_size=30
+    font = ImageFont.truetype(custom_font_path, font_size)
+# Use textbbox instead of textsize
+    bbox = draw.textbbox((0, 0), msg, font=font)
+    w, text_height = bbox[2] - bbox[0], bbox[3] - bbox[1]
     positions = [(((W-w)/2)-20,505),(480,180),(170,500),(480,800),(750,500),(350,430),(610,430),(350,630),(610,630)]
-    font_size = 30
     #if we have another font we can use this
     add_text_to_image(input_image_path, output_image_path, texts_to_add, positions, font_size, font_path=custom_font_path)
     #we can change the text size , font , and positions as needed 
